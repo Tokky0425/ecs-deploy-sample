acc90793b93b4e75530d788d0d3225be364d8f6a
kakakakakku <y.yoshida22@gmail.com>
2017-04-27 10:33:28 +0900

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 eval $(aws ecr get-login --region ap-northeast-1)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/test:$CIRCLE_SHA1
-./ecs-deploy --cluster test --service-name test-service --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/test:$CIRCLE_SHA1
+./ecs-deploy --cluster test-cluster --service-name test-service --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/test:$CIRCLE_SHA1
